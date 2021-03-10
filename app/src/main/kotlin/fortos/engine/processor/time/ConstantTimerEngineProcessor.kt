55ec7b5d8554070346b5241fc5e9ad5c3f55df83
João Paulo <joaolrpaulo@gmail.com>
2021-03-10 15:00:53 +0000

    Fixes to Constant Timer Processor, in order to have more predictable wait times
    Signed-off-by: João Paulo <joaolrpaulo@gmail.com>

--- app/src/main/kotlin/fortos/engine/processor/time/ConstantTimerEngineProcessor.kt
+++ app/src/main/kotlin/fortos/engine/processor/time/ConstantTimerEngineProcessor.kt
@@ -6,21 +6,19 @@ import java.time.Instant
 import java.util.concurrent.atomic.AtomicInteger
 
 class ConstantTimerEngineProcessor : BaseTimeEngineProcessor {
-    private val startedAt = Instant.now()
     private val transactionsPerformed = AtomicInteger()
 
     override fun call(input: Step): List<TimeEngineProcessorData> {
         val timerDefinition = input as ConstantTimerStep
-        val endTime = startedAt.plusSeconds(timerDefinition.duration)
-        val waitTime: Long =
-            if (timerDefinition.transactions == 1L) 0L
-            else timerDefinition.duration * 1000 / timerDefinition.transactions
+        val endTime = Instant.now().plusSeconds(timerDefinition.duration)
+        val maxTransactions = timerDefinition.transactions * timerDefinition.duration
+        val waitTime = (1000 / timerDefinition.transactions.toFloat()) * timerDefinition.threads.toFloat()
 
         return List(timerDefinition.threads.toInt()) {
             TimeEngineProcessorData(
-                execute = { if (transactionsPerformed.incrementAndGet() <= timerDefinition.transactions) it() },
-                shouldProceed = { Instant.now().isBefore(endTime) },
-                wait = { Thread.sleep(waitTime) }
+                execute = { if (transactionsPerformed.incrementAndGet() <= maxTransactions) it() },
+                shouldProceed = { Instant.now().isBefore(endTime) && transactionsPerformed.get() <= maxTransactions.toInt() },
+                wait = { Thread.sleep(waitTime.toLong()) }
             )
         }
     }
